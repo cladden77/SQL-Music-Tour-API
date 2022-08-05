deb094458e6b43eecd34b827554376557ff04ff5
Chris L <chris.ladden@gmail.com>
2022-08-04 21:02:46 -0500

    removed login example

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
-{
+require('dotenv').config()
+
+module.exports = {
   "development": {
-    "username": "postgres",
-    "password": "Postgres",
-    "database": "music_tour",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
