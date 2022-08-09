f55ba288c4a42e49182b53d89ab7e9e680ee97f5
Chris L <chris.ladden@gmail.com>
2022-08-09 18:13:10 -0500

    added in associations

--- models/event.js
+++ models/event.js
@@ -4,13 +4,18 @@ const {
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
-    /**
+      /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Stage, StageEvent }) {
+        // stages
+        Event.belongsToMany(Stage, {
+            foreignKey: "event_id",
+            as: "stages", 
+            through: StageEvent
+        })
     }
   }
   Event.init({
