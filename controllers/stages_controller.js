8eb0a1f0b7bf75e7dd91e2fedb516e8d3573e687
Chris L <chris.ladden@gmail.com>
2022-08-09 21:27:15 -0500

    updated association queries in events and stages controllers

--- controllers/stages_controller.js
+++ controllers/stages_controller.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const stages = require('express').Router()
 const db = require('../models')
-const { Stage } = db 
+const { Stage, Event } = db 
 const { Op } = require('sequelize')
 
 // FIND ALL STAGES
@@ -12,6 +12,7 @@ stages.get('/', async (req, res) => {
                 stage_name: { [Op.like]: `%${req.query.stage_name ? req.query.stage_name : ''}%` }
             }
         })
+
         res.status(200).json(foundStages)
     } catch (error) {
         res.status(500).json(error)
@@ -19,10 +20,18 @@ stages.get('/', async (req, res) => {
 })
 
 // FIND A SPECIFIC STAGE
-stages.get('/:id', async (req, res) => {
+stages.get('/:name', async (req, res) => {
     try {
         const foundStage = await Stage.findOne({
-            where: { stage_id: req.params.id }
+            where: { stage_name: req.params.name },
+            include:{ 
+                model: Event, 
+                as: "events",
+                through: { attributes: [] }
+            },
+            order: [
+                [{ model: Event, as: "events" }, 'date', 'ASC'],
+            ]
         })
         res.status(200).json(foundStage)
     } catch (error) {
