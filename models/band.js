e6198222c2b377a8b86c85e4bb80aa075de85ec6
Chris L <chris.ladden@gmail.com>
2022-08-09 20:53:31 -0500

    added in association queries

--- models/band.js
+++ models/band.js
@@ -4,16 +4,22 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Band extends Model {
-      /**
+    /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ MeetGreet }) {
-      // meet and greets 
+    static associate({ MeetGreet, SetTime }) {
+      // meet and greets
       Band.hasMany(MeetGreet, {
         foreignKey: "band_id",
         as: "meet_greets"
+      })
+
+      // set times 
+      Band.hasMany(SetTime, {
+        foreignKey: "band_id",
+        as: "set_times"
       })
     }
   }
--- models/settime.js
+++ models/settime.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-     static associate({ Band, Event, Stage }) {
+    static associate({ Band, Event, Stage }) {
       // band
       SetTime.belongsTo(Band, {
         foreignKey: "band_id",
